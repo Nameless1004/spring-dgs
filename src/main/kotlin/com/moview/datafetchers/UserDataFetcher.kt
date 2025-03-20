fa0ae664ea0920afaefc811f2bb5cd695f5e13c6
Nameless1004 <jjho1029@gmail.com>
2025-03-20 18:29:15 +0900

    Mutation Data Fetcher

--- src/main/kotlin/com/moview/datafetchers/UserDataFetcher.kt
+++ src/main/kotlin/com/moview/datafetchers/UserDataFetcher.kt
@@ -2,7 +2,9 @@ package com.moview.datafetchers
 
 import com.moview.entities.User
 import com.moview.repository.UserRepository
+import com.moview.types.AddUserInput
 import com.netflix.graphql.dgs.DgsComponent
+import com.netflix.graphql.dgs.DgsMutation
 import com.netflix.graphql.dgs.DgsQuery
 import com.netflix.graphql.dgs.InputArgument
 
@@ -17,5 +19,16 @@ class UserDataFetcher(
     ): User {
         return userRepository.findById(userId)
             .orElseThrow { RuntimeException("User not found!") }
+    }
+
+    @DgsMutation
+    fun addUser(
+        @InputArgument input: AddUserInput): User {
+        val user = User(
+            username = input.username,
+            email = input.email
+        )
+
+        return userRepository.save(user)
     }
 }
